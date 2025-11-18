45ae4ca523eba5f943f4841d030eacbd75f1db71
ifnuu01 <ifnuu01@gmail.com>
2025-11-18 21:57:25 +0800

    feat : add tech stack

---
+++ src/components/Tech.tsx
@@ -0,0 +1,51 @@
+function Tech() {
+
+    const techs = [
+        {
+            icon: (
+                <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2-2v2m8 0V6a2 2 0 012 2v6a2 2 0 01-2 2H8a2 2 0 01-2-2V8a2 2 0 012-2h8z" />
+                </svg>
+            )
+        },
+        {
+            icon: (
+                <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2-2v2m8 0V6a2 2 0 012 2v6a2 2 0 01-2 2H8a2 2 0 01-2-2V8a2 2 0 012-2h8z" />
+                </svg>
+            )
+        },
+        {
+            icon: (
+                <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2-2v2m8 0V6a2 2 0 012 2v6a2 2 0 01-2 2H8a2 2 0 01-2-2V8a2 2 0 012-2h8z" />
+                </svg>
+            )
+        },
+        {
+            icon: (
+                <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2-2v2m8 0V6a2 2 0 012 2v6a2 2 0 01-2 2H8a2 2 0 01-2-2V8a2 2 0 012-2h8z" />
+                </svg>
+            )
+        },
+    ]
+
+    return (
+        <div>
+            <div className="text-2xl md:text-4xl font-bold text-center text-primary mb-8 md:mb-16 bg-white w-fit mx-auto px-6 py-2">
+                <h1>Tech Stack <br /> I Use</h1>
+            </div>
+            {/* Content */}
+            <div className="flex flex-wrap gap-4 justify-center items-center">
+                {techs.map((tech, index) => (
+                    <div className="bg-white/10 p-4 text-white" key={index}>
+                        {tech.icon}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Tech
--- src/pages/public/About.tsx
+++ src/pages/public/About.tsx
@@ -1,10 +1,14 @@
 import Profile from "../../components/Profile";
+import Tech from "../../components/Tech";
+import Timeline from "../../components/Timeline";
 import PublicLayout from "../../layouts/PublicLayout";
 
 export default function About() {
     return (
         <PublicLayout>
             <Profile />
+            <Timeline />
+            <Tech />
         </PublicLayout>
     )
 }
