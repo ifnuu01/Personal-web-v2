7f0be9eef1d2c6fa386a8cd38ed59e8a9fca9755
ifnuu01 <ifnuu01@gmail.com>
2025-12-23 16:01:37 +0800

    feat: upadate implement react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router"
+import { Outlet } from "react-router"
 import Navbar from "./components/Navbar"
-import About from "./pages/public/About"
-import Project from "./pages/public/Project"
-import Achievement from "./pages/public/Achievement"
-import Blog from "./pages/public/Blog"
-import Resume from "./pages/public/Resume"
 import { AnimatePresence } from 'framer-motion';
-import withTransition from "./utils/withTransition"
-import Login from "./pages/auth/Login";
-import Dashboard from "./pages/private/Dashboard";
-import Category from "./pages/private/Category";
-import Tech from "./pages/private/Tech";
-import Experience from "./pages/private/Experience";
-import Certificate from "./pages/private/Certificate";
-import ProjectManage from "./pages/private/Project";
-import BlogManage from "./pages/private/Blog"
-import GuestRoute from "./components/GuestRoute"
-import ProtectedRoute from "./components/ProtectedRoute"
 import Footer from "./components/Footer"
 import ScrollToTop from "./components/ScrollToTop"
 
-const AboutWithTransition = withTransition(About);
-const ProjectWithTransition = withTransition(Project);
-const AchievementWithTransition = withTransition(Achievement);
-const BlogWithTransition = withTransition(Blog);
-const ResumeWithTransition = withTransition(Resume);
-const LoginWithTransition = withTransition(Login);
-const DashboardWithTransition = withTransition(Dashboard);
-const CategoryWithTransition = withTransition(Category);
-const TechWithTransition = withTransition(Tech);
-const ExperienceWithTransition = withTransition(Experience);
-const CertificateWithTransition = withTransition(Certificate);
-const ProjectManageWithTransition = withTransition(ProjectManage);
-const BlogManageWithTransition = withTransition(BlogManage);
-
-
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <ScrollToTop />
       <AnimatePresence mode="wait">
         <div className="flex flex-col min-h-screen bg-pattern">
           <Navbar />
-          <Routes>
-            {/* Public */}
-            <Route path="/" element={<AboutWithTransition />} />
-            <Route path="/project" element={<ProjectWithTransition />} />
-            <Route path="/achievement" element={<AchievementWithTransition />} />
-            <Route path="/blog" element={<BlogWithTransition />} />
-            <Route path="/resume" element={<ResumeWithTransition />} />
-
-            {/* Auth */}
-            <Route element={<GuestRoute />}>
-              <Route path="/login" element={<LoginWithTransition />} />
-            </Route>
-
-            {/* Private */}
-            <Route element={<ProtectedRoute />}>
-              <Route path="/dashboard" element={<DashboardWithTransition />} />
-              <Route path="/dashboard/category" element={<CategoryWithTransition />} />
-              <Route path="/dashboard/tech" element={<TechWithTransition />} />
-              <Route path="/dashboard/experience" element={<ExperienceWithTransition />} />
-              <Route path="/dashboard/certificate" element={<CertificateWithTransition />} />
-              <Route path="/dashboard/project" element={<ProjectManageWithTransition />} />
-              <Route path="/dashboard/blog" element={<BlogManageWithTransition />} />
-            </Route>
-          </Routes>
+          <Outlet />
           <Footer />
         </div>
       </AnimatePresence>
-    </BrowserRouter>
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,76 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './contexts/AuthContext.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import withTransition from './utils/withTransition.tsx'
+import Login from "./pages/auth/Login";
+import Dashboard from "./pages/private/Dashboard";
+import Category from "./pages/private/Category";
+import Tech from "./pages/private/Tech";
+import Experience from "./pages/private/Experience";
+import Certificate from "./pages/private/Certificate";
+import ProjectManage from "./pages/private/Project";
+import BlogManage from "./pages/private/Blog"
+import GuestRoute from "./components/GuestRoute"
+import ProtectedRoute from "./components/ProtectedRoute"
+import About from './pages/public/About.tsx'
+import Project from './pages/public/Project.tsx'
+import Achievement from './pages/public/Achievement.tsx'
+import Blog from './pages/public/Blog.tsx'
+import Resume from './pages/public/Resume.tsx'
+
+const AboutWithTransition = withTransition(About);
+const ProjectWithTransition = withTransition(Project);
+const AchievementWithTransition = withTransition(Achievement);
+const BlogWithTransition = withTransition(Blog);
+const ResumeWithTransition = withTransition(Resume);
+const LoginWithTransition = withTransition(Login);
+const DashboardWithTransition = withTransition(Dashboard);
+const CategoryWithTransition = withTransition(Category);
+const TechWithTransition = withTransition(Tech);
+const ExperienceWithTransition = withTransition(Experience);
+const CertificateWithTransition = withTransition(Certificate);
+const ProjectManageWithTransition = withTransition(ProjectManage);
+const BlogManageWithTransition = withTransition(BlogManage);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <AboutWithTransition /> },
+      { path: "project", element: <ProjectWithTransition /> },
+      { path: "achievement", element: <AchievementWithTransition /> },
+      { path: "blog", element: <BlogWithTransition /> },
+      { path: "resume", element: <ResumeWithTransition /> },
+
+      {
+        element: <GuestRoute />,
+        children: [
+          { path: 'login', element: <LoginWithTransition /> }
+        ]
+      },
+
+      {
+        element: <ProtectedRoute />,
+        children: [
+          { path: "dashboard", element: <DashboardWithTransition /> },
+          { path: "dashboard/category", element: <CategoryWithTransition /> },
+          { path: "dashboard/tech", element: <TechWithTransition /> },
+          { path: "dashboard/experience", element: <ExperienceWithTransition /> },
+          { path: "dashboard/certificate", element: <CertificateWithTransition /> },
+          { path: "dashboard/project", element: <ProjectManageWithTransition /> },
+          { path: "dashboard/blog", element: <BlogManageWithTransition /> },
+        ]
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <RouterProvider router={router} />
     </AuthProvider>
   </StrictMode>,
 )
